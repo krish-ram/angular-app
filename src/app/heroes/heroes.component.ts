7f28f57227f1f79628a08ee2a68f402b36690429
Ram Krishna <krishram101@gmail.com>
2018-01-19 00:06:29 +0530

    adding messages component and services for data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,21 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes= HEROES;
-  selectedHero: Hero;
+    heroes: Hero[];
+    selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-  }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 
-  constructor() { }
+    getHeroes(): void {
+        this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+    }
+    constructor(private heroService: HeroService) { }
 
-  ngOnInit() { }
+    ngOnInit() {
+        this.getHeroes();
+    }
 
 }
